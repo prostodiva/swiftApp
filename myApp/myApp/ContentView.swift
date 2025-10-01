54715632a7e86ad10c1aec4d9b035929f0ebdad4
prostodiva <rkattsyna@mail.ru>
2025-09-30 22:55:03 -0700

    Start Creating Dashboard UI with SwiftUI

--- myApp/myApp/ContentView.swift
+++ myApp/myApp/ContentView.swift
@@ -8,17 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var searchText = ""
+    
+    //mock data for testing
+    let tasks = [
+        "Buy groceries 11:00am",
+        "Call mom 2:00pm",
+        "Finish project 5:00pm",
+        "Gym workout 7:00pm"
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 0.82, green: 0.76, blue: 0.71)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                TextField("Search Bar...", text: $searchText)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(25)
+                    .padding(15)
+                List(tasks, id: \.self) { task in
+                    HStack {
+                        Image(systemName: "circle")
+                            .foregroundColor(.white)
+                        Text(task)
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                    .listRowBackground(Color.clear)
+                }
+                .scrollContentBackground(.hidden)
+            }
         }
-        .padding()
     }
 }
-
+    
 #Preview {
     ContentView()
 }
